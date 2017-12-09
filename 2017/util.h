2858e28226fd5d39358f317940cc06eadf303381
Andrew F <flamewave000@live.com>
2017-12-09 14:57:59 -0500

    Completed Day 3's first puzzle

--- 2017/util.h
+++ 2017/util.h
@@ -8,7 +8,11 @@
 #include <streambuf>
 #include <sstream>
 #include <vector>
+#include "../stringx/include/stringx.hpp"
 
+inline std::string get_file(const std::string& filename) {
+    return get_file(filename.c_str());
+}
 std::string get_file(const char * filename) {
     std::ifstream ifs(filename);
     std::string content((std::istreambuf_iterator<char>(ifs)),
@@ -22,13 +26,22 @@ struct config {
     std::string input;
 };
 
-config proc(int argc, const char** argv) {
-    if(argc < 3 || argc > 4) exit(-1);
+config proc(int argc, const char** argv, int day) {
+    if(argc != 2 && argc != 4) exit(-1);
     std::string puzzle = argv[1];
     if(puzzle != "-1" && puzzle != "-2") exit(-1);
+    int p = puzzle[1] - '0';
+    if(argc == 2) {
+        return { p, get_file(strx::format("data/day{0}.txt") % day % strx::endf) };
+    }
+    std::string dataFlag = argv[2];
     return {
         puzzle == "-1" ? 1 : 2,
-        std::string(argv[2]) == "-d" ? std::string(argv[3]) : get_file(argv[2])
+        dataFlag == "-d"
+            ? std::string(argv[3])
+            : dataFlag == "-f"
+                ? get_file(argv[2])
+                : get_file(strx::format("data/day{0}.txt") % day % strx::endf)
     };
 }
 
