97cf2eb21bc6f58e308d8ff6400f9740b8e59690
Andrew F <flamewave000@live.com>
2017-12-11 10:35:17 -0500

    Fixed program CLI args processor

--- 2017/util.h
+++ 2017/util.h
@@ -10,10 +10,8 @@
 #include <vector>
 #include "../stringx/include/stringx.hpp"
 
-inline std::string get_file(const std::string& filename) {
-    return get_file(filename.c_str());
-}
-std::string get_file(const char * filename) {
+std::string get_file(const char *filename)
+{
     std::ifstream ifs(filename);
     std::string content((std::istreambuf_iterator<char>(ifs)),
                         (std::istreambuf_iterator<char>()));
@@ -21,28 +19,30 @@ std::string get_file(const char * filename) {
     return content;
 }
 
-struct config {
+struct config
+{
     int puzzle;
     std::string input;
 };
 
-config proc(int argc, const char** argv, int day) {
-    if(argc != 2 && argc != 4) exit(-1);
+config proc(int argc, const char **argv, int day)
+{
+    if (argc != 2 && argc != 4)
+        exit(-1);
     std::string puzzle = argv[1];
-    if(puzzle != "-1" && puzzle != "-2") exit(-1);
+    if (puzzle != "-1" && puzzle != "-2")
+        exit(-1);
     int p = puzzle[1] - '0';
-    if(argc == 2) {
-        return { p, get_file(strx::format("data/day{0}.txt") % day % strx::endf) };
-    }
+    if (argc == 2)
+        return {p, get_file(("data/day{0}.txt"_f % day % strx::endf).c_str())};
+
     std::string dataFlag = argv[2];
-    return {
-        puzzle == "-1" ? 1 : 2,
-        dataFlag == "-d"
-            ? std::string(argv[3])
-            : dataFlag == "-f"
-                ? get_file(argv[2])
-                : get_file(strx::format("data/day{0}.txt") % day % strx::endf)
-    };
+    return {p,
+            dataFlag == "-d"
+                ? std::string(argv[3])
+                : dataFlag == "-f"
+                      ? get_file(argv[3])
+                      : get_file(("data/day{0}.txt"_f % day % strx::endf).c_str())};
 }
 
 #endif
