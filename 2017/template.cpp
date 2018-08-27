70d6ea7b5ab7725ae1b708b495ff9b6c1fed98b0
Andrew Fryer <flamewave000@live.com>
2018-08-27 14:18:57 -0400

    Minor adjustment to code template

--- 2017/template.cpp
+++ 2017/template.cpp
@@ -4,8 +4,13 @@ using namespace std;
 using namespace strx;
 using namespace linq;
 
-int puzzle_a(const string &input);
-int puzzle_b(const string &input);
+int puzzle_a(const string &input)
+{
+}
+
+int puzzle_b(const string &input)
+{
+}
 
 int main(int argc, const char *argv[])
 {
@@ -14,14 +19,6 @@ int main(int argc, const char *argv[])
 	cout << "Data: " << c.input << endl;
 	cout << "\nResult:" << (c.puzzle == 1 ? puzzle_a(c.input) : puzzle_b(c.input)) << endl;
 	return 0;
-}
-
-int puzzle_a(const string &input)
-{
-}
-
-int puzzle_b(const string &input)
-{
 }
 
 /*
