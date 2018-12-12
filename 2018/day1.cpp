3671d10d69f803b53c5725799f0fdbb4fff3f989
Andrew Fryer <flamewave000@live.com>
2018-12-12 08:41:49 -0500

    Completed 2018 Day 1's second puzzle

--- 2018/day1.cpp
+++ 2018/day1.cpp
@@ -6,26 +6,47 @@ using namespace linq;
 
 int puzzle_a(const string &input)
 {
-    istringstream ss(input);
-    int frequency = 0;
-    int delta = 0;
-    while(!ss.eof()) {
-        ss >> delta;
-        frequency += delta;
-    }
-    return frequency;
+	istringstream ss(input);
+	int frequency = 0;
+	int delta = 0;
+	while (!ss.eof())
+	{
+		ss >> delta;
+		frequency += delta;
+	}
+	return frequency;
 }
 
 int puzzle_b(const string &input)
 {
-    return 0;
+	vector<int> deltas;
+	{
+		istringstream ss(input);
+		int delta = 0;
+		while (!ss.eof())
+		{
+			ss >> delta;
+			deltas.push_back(delta);
+		}
+	}
+	{
+		unordered_set<int> freqs;
+		int frequency = 0;
+		for (size_t c = 0, size = deltas.size(); true; c = (c + 1) % size)
+		{
+			frequency += deltas[c];
+			if (freqs.find(frequency) != freqs.end())
+				return frequency;
+			freqs.insert(frequency);
+		}
+	}
+	return INT32_MAX;
 }
 
 int main(int argc, const char *argv[])
 {
 	config c = proc(argc, argv, __FILE__);
 
-	cout << "Data: " << c.input << endl;
 	auto result = c.puzzle == 1 ? puzzle_a(c.input) : puzzle_b(c.input);
 	cout << "\nResult: " << result << endl;
 	return 0;
@@ -64,5 +85,31 @@ Here are other example situations:
 Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
 
 Your puzzle answer was 486.
+
+--- Part Two ---
+You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
+
+For example, using the same list of changes above, the device would loop as follows:
+
+ - Current frequency  0, change of +1; resulting frequency  1.
+ - Current frequency  1, change of -2; resulting frequency -1.
+ - Current frequency -1, change of +3; resulting frequency  2.
+ - Current frequency  2, change of +1; resulting frequency  3.
+ - (At this point, the device continues from the start of the list.)
+ - Current frequency  3, change of +1; resulting frequency  4.
+ - Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+
+In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the middle of processing the list.
+
+Here are other examples:
+
+ - +1, -1 first reaches 0 twice.
+ - +3, +3, +4, -2, -4 first reaches 10 twice.
+ - -6, +3, +8, +5, -6 first reaches 5 twice.
+ - +7, +7, -2, -7, -4 first reaches 14 twice.
+
+What is the first frequency your device reaches twice?
+
+Your puzzle answer was 69285.
 
 */
